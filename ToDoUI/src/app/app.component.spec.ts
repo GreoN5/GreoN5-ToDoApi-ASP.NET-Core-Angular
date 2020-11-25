48d35eccc733c57450e7e2598fe421b0654195dc
Georgi Dimitrov Dimitrov <60718032+GreoN5@users.noreply.github.com>
2020-11-25 18:36:38 +0200

    Fixing bugs in unit testing

--- ToDoUI/src/app/app.component.spec.ts
+++ ToDoUI/src/app/app.component.spec.ts
@@ -27,11 +27,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ToDoUI');
   });
-
- // it('should render title', () => {
-   // const fixture = TestBed.createComponent(AppComponent);
-    //fixture.detectChanges();
-    //const compiled = fixture.nativeElement;
-    //expect(compiled.querySelector('.content span').textContent).toContain('ToDoUI app is running!');
-  //});
 });
